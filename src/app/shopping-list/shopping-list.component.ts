b11f94d745ee966220f5d55206c66fc93e5b060b
DavideMarietti <76477890+DavideMarietti@users.noreply.github.com>
2023-04-25 22:24:18 +0200

    added template driven forms.

--- src/app/shopping-list/shopping-list.component.ts
+++ src/app/shopping-list/shopping-list.component.ts
@@ -27,6 +27,10 @@ export class ShoppingListComponent implements OnInit, OnDestroy {
       )
   }
 
+  onEditItem(index: number) {
+    this.slServices.startedEditing.next(index);
+  }
+
   ngOnDestroy(): void {
     this.igChangeSub.unsubscribe();
   }
